f40580ca4fdb03339ad9f390d75d6785e8f88bf1
LeoMarzoli <leo.marzoli@ainexxo.com>
2026-03-04 10:28:05 +0100

    fix: now the default of the sdk in config.py is from CI first and the uses prod.

--- tests/test_base_functionality.py
+++ tests/test_base_functionality.py
@@ -1,13 +1,16 @@
+import os
+
 import httpx
 import pytest
 
 from neurolinker_sdk import AsyncNeuroLinker
 
-
+TOKEN = os.getenv("NEUROLINKER_API_KEY")
+    
 @pytest.mark.asyncio
 async def test_base_functionality() -> None:
     """Test that the NeuroLinker client can be instantiated and make a simple API call."""
-    async with AsyncNeuroLinker(token="nl_CGd6bozHhmU4asxkd-VlESSG8vep1Anm") as client:
+    async with AsyncNeuroLinker(token=TOKEN) as client:
         tasks = await client.tasks.list()
         assert isinstance(tasks, dict)
         assert "success" in tasks
